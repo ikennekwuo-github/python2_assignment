012e4b5c4739ad95c3c6456f2fc926c60f83d048
ikennekwuo-github <ikeemmanuellike@gmail.com>
2024-08-30 14:15:11 +0530

    python_assignment

---
+++ list.py
@@ -0,0 +1,15 @@
+my_list = [10, 20, 30, 40]
+my_list.insert(1, 15)
+print(my_list)
+
+
+my_list = [10, 20, 30, 40]
+my_list.extend([50, 60, 70])
+print(my_list)
+
+my_list = [10, 20, 30, 40]
+my_list.pop()
+print(my_list)
+
+my_list = [10, 20, 30, 40]
+print(my_list[2])
